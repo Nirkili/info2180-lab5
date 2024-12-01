34f29554dced60cb6c58f4f984caf54848d83723
Tara_Donald <taralee.kd.db@gmail.com>
2024-11-30 23:44:08 -0500

    Finished Exercise 5

--- world.js
+++ world.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", (e) =>{
     const php_req = new XMLHttpRequest();
     const php_file = 'http://localhost/info2180-lab5/world.php';
     var searchButton = document.getElementById('lookup');
+    var searchButton1 = document.getElementById('cityLookup');
     var searchBar = document.getElementById('country');
     var result = document.getElementById('result');
 
-    function fetchReq(txt){
-        fetch(php_file + "?country=" + encodeURIComponent(txt)).then(
+    function fetchReq(txt, option){
+        fetch(php_file + "?country=" + encodeURIComponent(txt) + "&lookup=" + encodeURIComponent(option)).then(
             response => response.text())
             .then(data => {
 
@@ -17,9 +18,14 @@ document.addEventListener("DOMContentLoaded", (e) =>{
             });
     }
 
+    searchButton1.addEventListener('click', (e) =>{
+        txt = searchBar.value.trim();
+        fetchReq(txt,'cities');
+    });
+
     searchButton.addEventListener('click', (e) =>{
         txt = searchBar.value.trim();
-        fetchReq(txt);
+        fetchReq(txt,'');
     });
 
 });
--- world.php
+++ world.php
@@ -4,6 +4,8 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
 if (isset($_GET['country'])) {
   $query = strip_tags($_GET['country']);
 } 
@@ -11,33 +13,79 @@ else {
   $query = '';
 }
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-
-if($query == ''){
-  $stmt = $conn->query("SELECT * FROM countries");
+if (isset($_GET['lookup'])) {
+  $query2 = strip_tags($_GET['lookup']);
+} 
+else {
+  $query2 = '';
 }
-else{
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$query%'");
-}
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<?php
 
-
-<table>
-  <th>Name</th>
-  <th>Continent</th>
-  <th>Independence</th>
-  <th>Head of State</th>
-<?php 
-  
-  foreach ($results as $row): ?>
+if($query2 == 'cities'){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON countries.code = cities.country_code WHERE countries.name LIKE '%$query%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+  <table>
     <tr>
-    <td><?= $row['name'] ?></td>
-    <td><?= $row['continent'] ?></td>
-    <td><?= $row['independence_year'] ?></td>
-    <td><?= $row['head_of_state'] ?></td>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
     </tr>
-<?php endforeach; ?>
-</table>
+  <?php 
+    
+    foreach ($results as $row): ?>
+      <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+      </tr>
+  <?php endforeach; ?>
+  </table>
+  <?php }
+else{
+  if($query == ''){
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>    
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+      </tr>
+    <?php 
+      
+      foreach ($results as $row): ?>
+        <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['continent'] ?></td>
+        <td><?= $row['independence_year'] ?></td>
+        <td><?= $row['head_of_state'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </table>
+    <?php }
+
+  else{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$query%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+      </tr>
+    <?php 
+      
+      foreach ($results as $row): ?>
+        <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['continent'] ?></td>
+        <td><?= $row['independence_year'] ?></td>
+        <td><?= $row['head_of_state'] ?></td>
+        </tr>
+    <?php endforeach;}} ?>
+    </table>
